e0f37a34b0073cc3561d528cee3ed66741d1d627
Elaine Chan <elaineeechan@gmail.com>
2018-12-04 22:12:07 -0500

    Add movement to the bug

--- js/app.js
+++ js/app.js
@@ -8,6 +8,8 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
     this.x = 0;
     this.y = 0;
+    this.step = 101;
+    this.boundary = this.step * 5;
 };
 
 // Update the enemy's position, required method for game
@@ -16,6 +18,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    // checks if bug is within boandary
+    // moves bug forward by speed * dt
+    if (this.x < this.boundary) {
+      this.x += 200 * dt;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
